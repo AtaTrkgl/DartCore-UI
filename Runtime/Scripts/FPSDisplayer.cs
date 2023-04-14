bb6df6f15494cbe1a4d3ef9863eafd255c1e713d
Ata Türkoğlu <ataturkoglu2003@hotmail.com>
2023-04-14 23:20:55 +0300

    FPS Low

--- Runtime/Scripts/FPSDisplayer.cs
+++ Runtime/Scripts/FPSDisplayer.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 using UnityEditor;
 #endif
 
-namespace  DartCore.UI
+namespace DartCore.UI
 {
     public class FPSDisplayer : MonoBehaviour
     {
@@ -31,27 +31,37 @@ namespace  DartCore.UI
         public static FPSDisplayer instance;
         private static float avg = 0;
         private static float sceneAvg = 0;
+        private static float low = 0;
+        private static float sceneLow = 0;
         private static int sceneFrameCount = 0;
 
+        [Header("Toggle")]
         [SerializeField] private bool toggled = true;
         [SerializeField] private KeyCode toggleKey = KeyCode.F6;
 
+        [Header("Display Options")] 
+        [SerializeField] private bool displayAverage = true;
+        [SerializeField] private bool displayLow = true;
+        [SerializeField, Tooltip("Only the FPS after this amount of time passes after a level load will be counted as the lowest fps. This helps by avoiding the initial drops on level load, this way you can actually analyze the fps drops mid-level.")]
+        private float lowFpsMinTimeSinceLevelLoad = 1f;
+        [SerializeField] private bool displaySceneSpecific = true;
+        [Space]
         [SerializeField] private Color textColor = Color.green;
         private TMP_Text fpsText;
-    
+
         private void Awake()
         {
             if (instance)
                 Destroy(gameObject);
             else
                 instance = this;
-            
+
             fpsText = GetComponentInChildren<TMP_Text>();
-            
+
             DontDestroyOnLoad(this);
             UpdateColor(textColor);
 
-            SceneManager.activeSceneChanged += (_, __) => { sceneAvg = 0; sceneFrameCount = 0; };
+            SceneManager.activeSceneChanged += (_, __) => { sceneAvg = 0; sceneFrameCount = 0; sceneLow = 0; };
         }
 
         private void Update()
@@ -62,20 +72,37 @@ namespace  DartCore.UI
 
             var currentFPS = 1 / Time.unscaledDeltaTime;
             var roundedFPS = Mathf.RoundToInt(currentFPS);
-            
+
+            if (Time.timeSinceLevelLoad > lowFpsMinTimeSinceLevelLoad)
+            {
+                if (roundedFPS < low || low == 0) low = roundedFPS;
+                if (roundedFPS < sceneLow || sceneLow == 0) sceneLow = roundedFPS;
+            }
+
             sceneFrameCount++;
-            
+
             avg = (avg * (Time.frameCount - 1) + currentFPS) / Time.frameCount;
             sceneAvg = (sceneAvg * (sceneFrameCount - 1) + currentFPS) / sceneFrameCount;
-            
+
             if (!toggled)
             {
                 fpsText.text = "";
                 return;
             }
-            fpsText.text = $"FPS: {(roundedFPS < 100 ? roundedFPS + " " : roundedFPS.ToString())}"
-                           + $" Avg: {Mathf.RoundToInt(avg)}"
-                           + $" Scene Avg: {Mathf.RoundToInt(sceneAvg)}";
+
+            var textToDisplay = $"FPS: {(roundedFPS < 100 ? roundedFPS + " " : roundedFPS.ToString())}";
+            if (displayAverage || displayLow) textToDisplay += " |";
+            if (displayAverage) textToDisplay += $" Avg: {Mathf.RoundToInt(avg)}";
+            if (displayLow) textToDisplay += $" Low: {Mathf.RoundToInt(low)}";
+
+            if (displaySceneSpecific && (displayAverage || displayLow))
+            {
+                textToDisplay += " |";
+                if (displayAverage) textToDisplay += $" Scene Avg: {Mathf.RoundToInt(sceneAvg)}";
+                if (displayLow) textToDisplay += $" Scene Low: {Mathf.RoundToInt(sceneLow)}";
+            }
+
+            fpsText.text = textToDisplay;
         }
 
         public void UpdateColor(Color color)
